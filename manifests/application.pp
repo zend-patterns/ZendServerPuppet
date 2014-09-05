26fe1ec96088783393fd32ee6169ddfe3639f065
David Lowes <david.l@zend.com>
2014-09-05 10:13:47 +0300

    deployment - default target

--- manifests/application.pp
+++ manifests/application.pp
@@ -2,11 +2,12 @@
 #   Debian specific settings for Zend Server install
 #
 # TODO:auto require logo file and app_package
+# TODO:automatically repair broken apps (according to status and notifications)
 define zendserver::application (
   $ensure,
-  $target,
+  $target          = 'localadmin',
   $app_package     = undef,
-  $base_url        = 'http://<default-server>/',
+  $base_url        = '/',
   $create_vhost    = false,
   $default_server  = undef,
   $user_app_name   = $name,
@@ -14,47 +15,54 @@ define zendserver::application (
   $user_params     = '',
   $version         = undef,
   $logo            = '',) {
+  
   case $ensure {
-    'present', 'deployed' : {
+    'present', 'deployed'  : {
       zendserver::application::deploy { $name:
         target        => $target,
         app_package   => $app_package,
         base_url      => $base_url,
         create_vhost  => $create_vhost,
-        user_app_name => $name,
+        user_app_name => $user_app_name,
         user_params   => $user_params,
       }
     }
-    'defined'             : {
+    'defined'              : {
       zendserver::application::define { $name:
         target        => $target,
         base_url      => $base_url,
         version       => '1.0',
         logo          => $logo,
-        user_app_name => $name,
+        user_app_name => $user_app_name,
       }
     }
-    'absent'              : {
+    'absent', 'undeployed' : {
       zendserver::application::remove { $name:
         target        => $target,
         user_app_name => $user_app_name,
       }
     }
-    'latest'              : {
-      if defined("$::zend_application_name_${name}") {
-
+    'latest'               : {
+      $app_name_fact = getvar("::zend_application_name_${user_app_name}")
+      if $app_name_fact != undef {
+        zendserver::application::update { $name:
+          target        => $target,
+          app_package   => $app_package,
+          user_app_name => $user_app_name,
+          user_params   => $user_params,
+        }
       } else {
         zendserver::application::deploy { $name:
           target        => $target,
           app_package   => $app_package,
           base_url      => $base_url,
           create_vhost  => $create_vhost,
-          user_app_name => $name,
+          user_app_name => $user_app_name,
           user_params   => $user_params,
         }
       }
     }
-    default               : {
+    default                : {
     }
   }
 }
--- manifests/application/update.pp
+++ manifests/application/update.pp
@@ -1,28 +1,10 @@
-/* applicationUpdate --appId= --appPackage= [--ignoreFailures=] [--userParams=] [--target=] [--zsurl=] [--zskey=] [--zssecret=]
- * [--zsversion=] [--http=]
- *
- * This method allows you to update an existing application. The package you provide must contain the same application.
- * Additionally, any new parameters or new values for existing parameters must be provided. This process is asynchronous, meaning
- * the initial request will wait until the package is uploaded and verified, and the initial response will show information about
- * the new version being deployed. However, the staging and activation process will proceed after the response is returned. You must
- * continue checking the application status using the applicationGetStatus method until the deployment process is complete.
- * --appId             The application ID you would like to update.
- * --appPackage        The application package file.
- * --ignoreFailures    Ignore failures during staging if only some servers report failures. If all servers report failures the
- * operation will fail in any case.
- *                    The default value is FALSE, meaning any failure will return an error.
- * --userParams        Set values for user parameters defined in the package. Depending on package definitions, this parameter may
- * be required.
- *                    Each user parameter defined in the package must be provided as a key for this parameter.
- * Example:            applicationUpdate --appId=3 --appPackage=/path/to/application-v0.3.zpk
- * --userParams="APPLICATION_ENV=staging&p[a]=1&p[b]=2"
- */
-
-
 define zendserver::application::update ($target, $app_package, $user_app_name = $name, $user_params = undef,) {
   # TODO: check application version in zpk against deployed
   # Check if application is deployed by using facter
-  if defined("$::zend_application_name_${name}") {
+  $app_name_fact = getvar("::zend_application_name_${user_app_name}")
+
+  # Check if application is deployed by using facter
+  if $app_name_fact != undef {
     # Get application id from facter
     $app_id           = getvar("::zend_application_id_${user_app_name}")
     $required_options = "--appId=${app_id} --appPackage=${app_package}"
