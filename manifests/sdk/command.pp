71ff2d88d795498352112b1aa6b198e33df9bbb4
David Lowes <david.l@zend.com>
2014-10-07 17:15:07 +0300

    SDK - retry

--- manifests/sdk/command.pp
+++ manifests/sdk/command.pp
@@ -1,15 +1,42 @@
-#TODO: use params class pattern
-define zendserver::sdk::command ( $target,
-                                  $api_command  = $name,
-                                  $zsurl    = undef,
-                                  $zskey    = undef,
-                                  $zssecret = undef,
-                                  $http_timeout = 60,
-                                  $additional_options = '',
-                                  $tries = 3,
-                                  $try_sleep = 5){
-    exec {"zsapi_${name}":
-    path    => "/usr/local/zend/bin:${::path}",
-    command => "zs-client.phar ${api_command} --target=${target} ${additional_options} "
+# == Definition: zendserver::sdk::command
+# Execute a Zend Server web api function using the next generation zend server SDK (zs-client.phar)
+# You can read more about the SDK at https://github.com/zend-patterns/ZendServerSDK
+# This define authenticates against the Zend Server api
+# === Parameters
+# [*target*]
+# The target which the command will be run against - zendserver::bootstrap::exec defines a target named 'localamdin' pointing to the local server
+# You can define more targets using zendserver::sdk:target
+# [*api_command*]
+# The api command to run (Default: $name). See /usr/local/zend/bin/zs-client.phar command:all for a list of commands to run
+# [*zsurl*]
+# The URL for the Zend Server API (not necessary if a target is defined)
+# [*zskey*] 
+# Zend Server API key name (not necessary if a target is defined)
+# [*zssecret*] 
+# Zend Server API key secret hash (not necessary if a target is defined)
+# [*http_timeout*] 
+# Timeout for accessing the Zend Server web API in seconds (Default :60)
+# [*additional_options*]
+# Additional options to pass to the web api client such as parameters for the call.
+# [*tries*]
+# Number of times to retry the request if failed. (Default: 3)
+# [*try_sleep*]
+# Number of seconds to sleep between retries. (Default: 5)
+  
+define zendserver::sdk::command (
+  $target,
+  $api_command        = $name,
+  $zsurl              = undef,
+  $zskey              = undef,
+  $zssecret           = undef,
+  $http_timeout       = 60,
+  $additional_options = '',
+  $tries              = 3,
+  $try_sleep          = 5,) {
+  exec { "zsapi_${name}":
+    path      => "/usr/local/zend/bin:${::path}",
+    tries     => $tries,
+    try_sleep => $try_sleep,
+    command   => "zs-client.phar ${api_command} --target=${target} ${additional_options} ",
   }
 }
