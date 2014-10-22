3d9b1fec72ec3651478def73969b89391bff0300
David Lowes <david.l@zend.com>
2014-10-22 14:58:01 +0300

    Library Deployment - Document and lint the code.

--- manifests/library.pp
+++ manifests/library.pp
@@ -1,6 +1,21 @@
 # == Definition: zendserver::library
-#   Debian specific settings for Zend Server install
-#
+# Deploy a Zend Server library. 
+# === Parameters
+# [*ensure*]
+# present, deployed, latest - deploy a zpk package to the selected target.
+# absent, undeployed - remove and application from the selected target.
+# defined - define an existing application (which was deployed by other means) in Zend Server.
+# [*target*]
+# Zend Server SDK target from which to remove the application.
+# [*lib_package*]
+# The name of the application package (zpk) to deploy.
+# [*user_lib_name*]
+# The user librarie's name (alias) (Default: resource name)
+# [*version*]
+# The version of the library(Optional).
+# [*logo*]
+# Path to a local file on the server holding the library's logo (for displaying in the Zend Server console).
+# It is advised you add a puppet "File" resource for the logo file and "require" it.
 # TODO:auto require lib_package
 # TODO:automatically repair broken libraries (according to status and notifications)
 define zendserver::library (
--- manifests/library/deploy.pp
+++ manifests/library/deploy.pp
@@ -1,11 +1,21 @@
+# == Define: zendserver::library::deploy
+# Internal definition to deploy a Zend Server Library using the Zend Server SDK.g
+#This defined type should not be called directly in a user's manifest.
+#
+# === Parameters
+# [*target*]
+# Zend Server SDK target from which to remove the library.
+# [*lib_package*]
+# Zend Server Library name to remove.
+
 define zendserver::library::deploy ($lib_package, $target,) {
   $required_options = "--zpk=${lib_package}"
   $additional_options = ''
 
   # Check if application is deployed by using facter
-  zendserver::sdk::command { "lib_deploy_$name":
+  zendserver::sdk::command { "lib_deploy_${name}":
     target             => $target,
-    api_command        => 'installApp',
+    api_command        => 'installLib',
     additional_options => "${required_options} ${additional_options}",
   }
 }
