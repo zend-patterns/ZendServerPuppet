dc63135da7fc2f368fb8d71f6f868905c0613f0a
Ugo Bellavance <ubellavance@atq.qc.ca>
2017-04-26 12:26:40 -0400

    Fixes Issue #15 - Cannot install on RHEL6.

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -6,8 +6,19 @@ class zendserver::repo::redhat inherits zendserver {
     warning("${name} should not be directly included in the manifest.")
   }
 
+ unless ($::osfamily == 'RedHat') and (versioncmp($::operatingsystemrelease, '6.0') >= 0) {
+    fail("OS family ${::osfamily}-${::operatingsystemrelease} is not supported. Only RedHat >= 6 is suppported. RHEL 5 is EOL")
+  }
+
+  $rpmdir = $::operatingsystemrelease ? {
+    /^5.*$/ => 'rpm',
+    /^6.*$/ => 'rpm',
+    /^7.*$/ => 'rpm_apache2.4',
+  }
+    
+
   yumrepo { 'Zend':
-    baseurl  => "http://repos.zend.com/zend-server/${zendserver::zend_server_version}/rpm_apache2.4/${::architecture}",
+    baseurl  => "http://repos.zend.com/zend-server/${zendserver::zend_server_version}/${rpmdir}/${::architecture}",
     descr    => 'Zend Server',
     enabled  => 1,
     gpgcheck => 1,
@@ -15,7 +26,7 @@ class zendserver::repo::redhat inherits zendserver {
   }
 
   yumrepo { 'Zend_noarch':
-    baseurl  => "http://repos.zend.com/zend-server/${zendserver::zend_server_version}/rpm_apache2.4/noarch",
+    baseurl  => "http://repos.zend.com/zend-server/${zendserver::zend_server_version}/${rpmdir}/noarch",
     descr    => 'Zend Server',
     enabled  => 1,
     gpgcheck => 1,
