a72cc40bf3421114b58c98581c684c5bb0b97aff
Ugo Bellavance <ubellavance@atq.qc.ca>
2017-06-23 17:06:40 -0400

    Delete package.pp

--- manifests/extension/package.pp
+++
@@ -1,6 +0,0 @@
-# == Defined type: zendserver::extension::package
-#   Install package from the operating system repository
-# === Parameters
-
-define zendserver::extension::package ($package_name = '') {
-}
