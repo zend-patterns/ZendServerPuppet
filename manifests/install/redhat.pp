3836d91b5a8a4f974bccb0ef80662f2fbb506123
Brian Schonecker <bschonecker@healthplan.com>
2017-07-05 13:33:16 -0400

    Added conditional check for manage_repos so that we don't have a dependency when we don't need one.

--- manifests/install/redhat.pp
+++ manifests/install/redhat.pp
@@ -1,6 +1,11 @@
 # == Class: zendserver::install::redhat
 #  RedHat specific settings for Zend Server install
 class zendserver::install::redhat inherits zendserver::install {
-  Package[$zendserver::install::zendserverpkgname] {
-    require => [Yumrepo['Zend'], Yumrepo['Zend_noarch']], }
+
+  # We don't need to set requirements if we aren't managing the repositories.
+  if $zendserver::manage_repos {
+    Package[$zendserver::install::zendserverpkgname] {
+      require => [Yumrepo['Zend'], Yumrepo['Zend_noarch']], }
+  }
+
 }
