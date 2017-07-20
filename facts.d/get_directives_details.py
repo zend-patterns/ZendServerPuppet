fd1fa555312991be45d277f11f0dc825086613d1
Ugo Bellavance <ubellavance@atq.qc.ca>
2017-07-19 20:47:35 -0400

    Fixed python scripts to avoid throwing an error on first run.

--- facts.d/get_directives_details.py
+++ facts.d/get_directives_details.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python
 
 import json
+import os.path
 import re
 import subprocess
 
 zs_api_config_file = '/.zsapi.ini'
 zs_api_target = 'localadmin'
 
-directives_details = subprocess.check_output(["/usr/local/zend/bin/zs-client.sh", "configurationDirectivesList", "--target=localadmin", "--output-format=json"])
+if os.path.isfile("/usr/local/zend/bin/zs-client.sh"):
 
-## Strip the PHP notices from the json
-directives_details = re.sub("Notice:.*\n", "", directives_details)
-## Strip the newlines from the json
-directives_details = re.sub("\n", "", directives_details)
+  directives_details = subprocess.check_output(["/usr/local/zend/bin/zs-client.sh", "configurationDirectivesList", "--target=localadmin", "--output-format=json"])
 
-f1=open('/tmp/puppet_zend_directives.txt', 'w')
-f1.write(directives_details)
+  ## Strip the PHP notices from the json
+  directives_details = re.sub("Notice:.*\n", "", directives_details)
+  ## Strip the newlines from the json
+  directives_details = re.sub("\n", "", directives_details)
 
-arr = json.loads(directives_details)
+  f1=open('/tmp/puppet_zend_directives.txt', 'w')
+  f1.write(directives_details)
 
-for directive in arr[u"responseData"][u"directives"]:
-  name = directive["name"]
-  for key, value in directive.iteritems():
-    if value and not isinstance(value, list) and not isinstance(value,dict):
-      print ('zend_directive_' + key + '_' + name + '=' + value)
+  arr = json.loads(directives_details)
+
+  for directive in arr[u"responseData"][u"directives"]:
+    name = directive["name"]
+    for key, value in directive.iteritems():
+      if value and not isinstance(value, list) and not isinstance(value,dict):
+        print ('zend_directive_' + key + '_' + name + '=' + value)
--- facts.d/get_extensions_details.py
+++ facts.d/get_extensions_details.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python
 
 import json
+import os.path
 import re
 import subprocess
 
 zs_api_config_file = '/.zsapi.ini'
 zs_api_target = 'localadmin'
 
-extensions_details = subprocess.check_output(["/usr/local/zend/bin/zs-client.sh", "configurationExtensionsList", "--target=localadmin", "--output-format=json"])
+if os.path.isfile("/usr/local/zend/bin/zs-client.sh"):
 
-## Strip the PHP notices from the json
-extensions_details = re.sub("Notice:.*\n", "", extensions_details)
-## Strip the newlines from the json
-extensions_details = re.sub("\n", "", extensions_details)
-#extensions_details = re.sub("^M", "", extensions_details)
+  extensions_details = subprocess.check_output(["/usr/local/zend/bin/zs-client.sh", "configurationExtensionsList", "--target=localadmin", "--output-format=json"])
 
-f1=open('/tmp/puppet_zend_extension.txt', 'w')
-f1.write(extensions_details)
+  ## Strip the PHP notices from the json
+  extensions_details = re.sub("Notice:.*\n", "", extensions_details)
+  ## Strip the newlines from the json
+  extensions_details = re.sub("\n", "", extensions_details)
 
-arr = json.loads(extensions_details)
+  arr = json.loads(extensions_details)
 
-#f2=open('/tmp/puppet_zend_extension_arr.txt', 'w')
-#f2.write(arr)
-
-for extension in arr[u"responseData"][u"extensions"]:
-  name = extension["name"]
-  for key, value in extension.iteritems():
-    if not isinstance(value, list):
-      print ('zend_extension_' + key + '_' + name + '=' + value)
+  for extension in arr[u"responseData"][u"extensions"]:
+    name = extension["name"]
+    for key, value in extension.iteritems():
+      if not isinstance(value, list):
+        print ('zend_extension_' + key + '_' + name + '=' + value)
